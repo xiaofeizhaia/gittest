84f6ea3876320b19b5c661da42e66181a1dedbe8
LMX <LMX@qq.com>
2022-03-03 10:13:55 +0800

    push commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("hot-fix test1");
+        System.out.println("push");
     }
 }
